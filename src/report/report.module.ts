759bacb0582e929b75a3d90e81cad1b8966c68ce
moisesvalles1 <mvalles1@students.kennesaw.edu>
2025-10-24 17:21:16 -0400

    Connected to MongoDB

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ReportController } from './report.controller';
 import { ReportService } from './report.service';
+import { Event, EventSchema }  from '../event/entity/event.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]),
+  ],
   controllers: [ReportController],
   providers: [ReportService]
 })
--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Event } from '../event/entity/event.schema';
 
 @Injectable()
 export class ReportService {
+    constructor(
+        @InjectModel(Event.name) private eventModel: Model<Event>,
+    ) {}
+
     async generateReport(eventId: string): Promise<Buffer> {
         //Mock data
-        const eventData = {
-            id: eventId,
-            title: 'KSU Career Fair',
-            date: '10-27-2025',
-            attendees: 100,
-            location: 'Student Success Center',
-        };
+        // const eventData = {
+        //     id: eventId,
+        //     title: 'KSU Career Fair',
+        //     date: '10-27-2025',
+        //     attendees: 100,
+        //     location: 'Student Success Center',
+        // };
+
+        //Fetch data from MongoDB
+        const event = await this.eventModel.findById(eventId).exec();
 
         //Create file
         const content = `
         Event Report
         -------------
-        Event ID: ${eventData.id}
-        Title: ${eventData.title}
-        Data: ${eventData.date}
-        Location: ${eventData.location}
-        Attendees: ${eventData.attendees}
+        Event ID: ${event!._id}
+        Title: ${event!.title}
+        Data: ${event!.eventDate}
+        Location: ${event!.location}
+        Attendees: ${event!.reservationCount}
         `;
 
         //Return
