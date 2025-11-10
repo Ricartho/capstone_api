29b3958421d54d6ea454c8bd27f132e500a777b0
moisesvalles1 <mvalles1@students.kennesaw.edu>
2025-11-10 17:33:08 -0500

    Updates to download api

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res, NotFoundException } from '@nestjs/common';
 import { Response } from 'express';
 import { ReportService } from './report.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Event } from '../event/entity/event.schema';
 
 @Controller('report')
 export class ReportController {
-    constructor(private readonly reportService: ReportService) {}
+    constructor(private readonly reportService: ReportService,
+        @InjectModel(Event.name) private readonly eventModel: Model<Event>,
+    ) {}
 
     @Get('download/all')
     async downloadAllReports(@Res() res: Response){
@@ -16,11 +21,24 @@ export class ReportController {
 
     @Get('download/:id')
     async downloadReport(@Param('id') id: string, @Res() res:Response){
+        //Retrieve event
+        if(!Types.ObjectId.isValid(id)){
+            throw new NotFoundException(`Invalid event id`);
+        }
+
+        const event = await this.eventModel.findById(id).exec();
+        if(!event) {
+            throw new NotFoundException(`Event not found`);
+        }
+
         //Create the file
         const fileBuffer = await this.reportService.generateReport(id);
 
+        //Clean title
+        const cleanTitle = event.title.replace(/[^a-z0-9_\-]/gi, '_');
+
         //Response header for the file download
-        res.setHeader('Content-Disposition', `attachment; filename=report-${id}.txt`);
+        res.setHeader('Content-Disposition', `attachment; filename=report-${cleanTitle}.txt`);
         res.setHeader('Content-Type', 'text/plain');
 
         //Send file
--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Event } from '../event/entity/event.schema';
@@ -11,30 +11,34 @@ export class ReportService {
 
     async generateAllReports(): Promise<Buffer> {
         //Mock data
-        const mockData = [ {
-            title: 'KSU Career Fair',
-            eventDate: '10-27-2025',
-            reservationCount: 100,
-            location: 'Student Success Center',
-        },
-        {
-            title: 'C-Day',
-            eventDate: '11-10-2025',
-            reservationCount: 100,
-            location: 'Student Success Center',
-        },
-        ];
+        // const mockData = [ {
+        //     title: 'KSU Career Fair',
+        //     eventDate: '10-27-2025',
+        //     reservationCount: 100,
+        //     location: 'Student Success Center',
+        // },
+        // {
+        //     title: 'C-Day',
+        //     eventDate: '11-10-2025',
+        //     reservationCount: 100,
+        //     location: 'Student Success Center',
+        // },
+        // ];
 
         //Fetch data from MongoDB
-        //const events = await this.eventModel.find().exec();
+        const events = await this.eventModel.find().exec();
 
+        if (!events || events.length === 0 ){
+            throw new NotFoundException('No events found');
+        }
 
         //Create report
         const header = 'All Events\n';
-        const reports = mockData
+        const reports = events
         .map((event,index) => {
             return `
             #${index + 1}
+            EventID: ${event.id}
             Title: ${event.title}
             Date: ${event.eventDate}
             Location: ${event.location}
@@ -45,34 +49,41 @@ export class ReportService {
 
         const fullReport = header + reports + '\n';
         return Buffer.from(fullReport, 'utf-8');
+        //http://localhost:3000/api/report/download/all
     }
 
     async generateReport(eventId: string): Promise<Buffer> {
         //Mock data
-        const eventData = {
-            id: eventId,
-            title: 'KSU Career Fair',
-            eventDate: '10-27-2025',
-            reservationCount: 100,
-            location: 'Student Success Center',
-        };
+        // const eventData = {
+        //     id: eventId,
+        //     title: 'KSU Career Fair',
+        //     eventDate: '10-27-2025',
+        //     reservationCount: 100,
+        //     location: 'Student Success Center',
+        // };
 
         //Fetch data from MongoDB
-        //const event = await this.eventModel.findById(eventId).exec();
+        const event = await this.eventModel.findById(eventId).exec();
+
+        if (!event) {
+            throw new NotFoundException(`Event not found`);
+        }
 
         //Create file
         const content = `
         Event Report
         -------------
-        Event ID: ${eventData.id}
-        Title: ${eventData.title}
-        Date: ${eventData.eventDate}
-        Location: ${eventData.location}
-        Reservation Count: ${eventData.reservationCount}
+        Event ID: ${event!.id}
+        Title: ${event!.title}
+        Date: ${event!.eventDate}
+        Location: ${event!.location}
+        Reservation Count: ${event!.reservationCount}
         `;
 
         //Return
         return Buffer.from(content, 'utf-8');
     
+        //http://localhost:3000/api/report/download/
+
     }
 }
